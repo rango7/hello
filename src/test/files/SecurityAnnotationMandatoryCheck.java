7830b99c39705e604e631c5d0977a48d38ba1279
cc <rango@188.com>
2017-11-30 14:34:59 +0800

    no message

---
+++ src/test/files/SecurityAnnotationMandatoryCheck.java
@@ -0,0 +1,22 @@
+package company.demo;
+
+/**
+ *This file is the sample code against we run our unit test.
+ *It is placed src/test/files in order to not be part of the maven compilation.
+ **/
+class BusinessClassDelegate implements MySecurityInterface, SecondInterface {
+
+
+
+  public  static  final  int aField;
+
+  @MySecurityAnnotation
+  public void aMethod() {
+
+  }
+
+  public void aMethod() { // Noncompliant {{Mandatory Annotation not set @MySecurityAnnotation}}
+
+  }
+
+}
